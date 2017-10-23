456a2ae86aa67466dd1b2f7b89bbf973db811722
Sam Singh <ssingh55@ucsc.edu>
2017-10-22 19:04:39 -0700

    started the swiping feature, about 50% done

--- clothr/clothr/ViewController.swift
+++ clothr/clothr/ViewController.swift
@@ -3,6 +3,11 @@
 //  clothr
 //
 //  Created by Czar Vince Gabriel Cabral on 10/21/17.
+//  Sam Singh
+//  Andrew Gu(i)terres
+//  Gilbert Aragon
+//  Kianna Mark
+//
 //  Copyright © 2017 cmps115. All rights reserved.
 //
 
@@ -10,9 +15,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swipeLabel: UILabel!
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+        
+        /*---------------------------------------
+         * Swiping Feature
+         ----------------------------------------*/
+        
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(wasDragged(gestureRecognizer:)))
+        swipeLabel.addGestureRecognizer(swipeGesture)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +34,28 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+    
+    // function that checks if card was dragged
+    @objc func wasDragged(gestureRecognizer: UIPanGestureRecognizer) {
+        let labelpoint = gestureRecognizer.translation(in: view)
+        swipeLabel.center = CGPoint(x: view.bounds.width / 2 + labelpoint.x , y: view.bounds.height / 2 + labelpoint.y)
+        
+        if gestureRecognizer.state == .ended {
+            if swipeLabel.center.x < (view.bounds.width / 2 - 100) {
+                print("NOT Interested")
+            }
+            
+            if swipeLabel.center.x > (view.bounds.width / 2 + 100) {
+                print("Interested in clothing item")
+            }
+            
+            swipeLabel.center = CGPoint(x: view.bounds.width/2 , y: view.bounds.height/2 )
+            
+        }
+        
+        //print(swipeLabel.center.x)
+        
+    }
+    
 }
 
