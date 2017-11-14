f03c90cefe7cbbd2f1c1a6c95b0e52e882b4cf5b
Gilbert Aragon <gilbertaragon7801@gmail.com>
2017-11-14 14:51:31 -0800

    user registry works

---
+++ clothr/clothr/clothr/LoginViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LoginViewController.swift
+//  clothr
+//
+//  Created by Gilbert Aragon on 11/14/17.
+//  Copyright © 2017 cmps115. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userUsername: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    var loginSuccess = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        errorLabel.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        if let username = userUsername.text {
+            if let password = userPassword.text {
+                PFUser.logInWithUsername(inBackground: username, password: password, block: { (user, error) in
+                    if error != nil {
+                        var errorMessage = "Login failed - Try Again"
+                        
+                        if let newError = error as NSError? {
+                            if let detailError = newError.userInfo["error"] as? String {
+                                errorMessage = detailError
+                            }
+                        }
+                        
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = errorMessage
+                        
+                    } else {
+                        print("Login successful")
+                        self.loginSuccess = true
+                        self.performSegue(withIdentifier: "loginToSwipe", sender: self)
+                    }
+                })
+            }
+        }
+    }
+}
---
+++ clothr/clothr/clothr/RegisterViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RegisterViewController.swift
+//  clothr
+//
+//  Created by Gilbert Aragon on 11/14/17.
+//  Copyright © 2017 cmps115. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var userUsername: UITextField!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    var registerSuccess = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        errorLabel.isHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func registerPressed(_ sender: Any) {
+        let user = PFUser()
+        user.username = userUsername.text
+        user.password = userPassword.text
+        user.email = userEmail.text
+        
+        user.signUpInBackground (block: { (success, error) in
+            if error != nil {
+                var errorMessage = "Sign Up failed - Try Again"
+                
+                if let newError = error as NSError? {
+                    if let detailError = newError.userInfo["error"] as? String {
+                        errorMessage = detailError
+                    }
+                }
+                
+                self.errorLabel.isHidden = false
+                self.errorLabel.text = errorMessage
+            } else {
+                print("Register successful")
+                self.registerSuccess = true
+                self.performSegue(withIdentifier: "registerToSwipe", sender: self)
+            }
+        })
+    }
+    
+}
