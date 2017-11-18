4caec7f0bccc7a077ac976d463365bc719b48d66
Gilbert Aragon <gilbertaragon7801@gmail.com>
2017-11-17 17:37:01 -0800

    Users can now delete their account in settings

--- clothr/clothr/clothr/SettingsViewController.swift
+++ clothr/clothr/clothr/SettingsViewController.swift
@@ -42,4 +42,36 @@ class SettingsViewController: UIViewController {
                 return
         }
     }
+    
+    @IBAction func deleteAccountPressed(_ sender: Any) {
+        // Create an alert text box
+        let alert = UIAlertController(title:"Warning", message:"Are you sure you want to delete your account?", preferredStyle: .alert)
+        
+        // Change font of the title and message
+        //let titleFont:[NSAttributedStringKey : Any] = [ NSAttributedStringKey(rawValue: NSAttributedStringKey.font.rawValue) : UIFont(name: "HelveticaNeue", size: 18)! ]
+        //let messageFont:[NSAttributedStringKey : Any] = [ NSAttributedStringKey(rawValue: NSAttributedStringKey.font.rawValue) : UIFont(name: "HelveticaNeue-Thin", size: 14)! ]
+        
+        // Yes button to delete account
+        let yes = UIAlertAction(title: "Yes", style: .destructive, handler: { (action) -> Void in
+            PFUser.current()?.deleteInBackground(block: { (deleteSuccessful, error) -> Void in
+                print("success = \(deleteSuccessful)")
+                PFUser.logOut()
+            })
+            
+            // go to home page after deleting account
+            guard (self.navigationController?.popToRootViewController(animated: true)) != nil
+                else {
+                    print("No View Controller to Pop")
+                    return
+            }
+        })
+        
+        // No button to not delete account
+        let no = UIAlertAction(title: "No", style: .default, handler: { (action) -> Void in })
+        
+        // Add the buttons and display the alert box
+        alert.addAction(yes)
+        alert.addAction(no)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
