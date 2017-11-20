32a32bd4f318b754e1f4cad82d881d8a43da9cd1
czarcabral <czar.cabral@gmail.com>
2017-11-20 03:11:44 -0800

    Remove navigation controller from login screen. Place navigation controller at main screen and onwards.

--- clothr/clothr/clothr/HomeViewController.swift
+++ clothr/clothr/clothr/HomeViewController.swift
@@ -22,15 +22,4 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // removes the navbar in the homepage
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        super.viewWillAppear(animated)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
-        super.viewWillDisappear(animated)
-    } 
-
 }
